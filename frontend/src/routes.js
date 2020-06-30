5cb242a01034021e09e1518cae947a4f37bc4b5a
TimHuang19 <timhuangetg@gmail.com>
2020-07-01 08:54:36 +1000

    Added basic login design

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,10 +3,11 @@ import { Route } from 'react-router-dom';
 import Home from './containers/Home';
 import Books from './containers/Books';
 import CollectionList from './containers/CollectionList';
+import Login from './containers/Login';
 
 const BaseRouter = () => (
   <div>
-    <Route exact path='/' component={Home}/>
+    <Route exact path='/' component={Login}/>
     <Route exact path='/books' component={Books}/>
     <Route exact path='/col_list' component={CollectionList}/>
   </div>
