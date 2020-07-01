6e2ef8bff72b79bec711b1072978936d19233757
Michael <michaelp1019@gmail.com>
2020-07-01 23:23:18 +1000

    List done

---
+++ frontend/src/containers/CollectionList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class CollectionList extends React.Component {
+
+    
+
+    render() {
+
+        return (
+            <div>
+                <h1>Collection List</h1>
+                <ul>
+                    <li>List 1</li>
+                    <li>List 2</li>
+                    <li>List 3</li>
+                </ul>,
+                
+            </div>
+        )
+    }
+}
