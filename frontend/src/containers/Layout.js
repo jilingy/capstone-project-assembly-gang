68075ca883b093e2aa83d1ec0038c4159c4758c2
Michael Wang <michael.mhw1@gmail.com>
2020-07-02 22:02:26 +1000

    edited button

--- frontend/src/containers/Layout.js
+++ frontend/src/containers/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout, Menu, Button } from 'antd';
 import { Link } from 'react-router-dom';
 
@@ -34,10 +34,10 @@ export default function CustomLayout(props) {
     <Layout className="site-layout" style={{ marginLeft: 200 }}>
       <Header className="site-layout-background" style={{ padding: 0 }}>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Button type="primary" style={{ left: 1420 }}><Link to="/register">Register</Link></Button>
-          <Button type="primary" style={{ left: 1440}}><Link to="/login">Login</Link></Button>
           <Button type="primary" style={{ float: 'right', marginTop: 15, marginRight: 20, backgroundColor: '#FF5833', borderColor: '#FF5833' }}><Link to="/logout">Logout</Link></Button>
           <Button type="primary" style={{ float: 'right', marginTop: 15, marginRight: 20 }}><Link to="/account">Account</Link></Button>
+          <Button type="primary" style={{ float: 'right', marginTop: 15, marginRight: 20 }}><Link to="/login">Login</Link></Button>
+          <Button type="primary" style={{ float: 'right', marginTop: 15, marginRight: 20 }}><Link to="/register">Register</Link></Button>
         </Menu>
       </Header>
       <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
