78c204673b2ebc381eec3d8f7b2ebe0604c6ad46
TimHuang19 <timhuangetg@gmail.com>
2020-07-15 21:51:00 +1000

    Book detail style changes

--- frontend/src/containers/BookDetail.js
+++ frontend/src/containers/BookDetail.js
@@ -1,21 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { Modal } from 'antd';
+import { WhiteSpace,Row,Col,Typography,Card ,Modal } from 'antd';
+import BookCover from '..//images/book_cover.jpg';
 //import axios from 'axios';
 import { connect } from 'react-redux';
 
 function BookDetail(props){
 
+    const {Title, Text} = Typography
+
     return(
       <div>
         <Modal
-          title="Basic Modal"
+          title={null}
           visible={props.show}
           footer={false}
           onCancel={props.hide}
-          mask={false}
+          mask={true}
           maskClosable={false}
+          maskStyle ={{marginLeft:200, marginTop:60}}
+          style ={{marginLeft:200,marginTop:-40}}
+          bodyStyle={{height: 882}}
+          width={1703}
         >
-          <p>{props.book_title}</p>
+          <Row gutter={16}>
+          <Col>
+            <Card 
+              style={{ width: 400, height: 400, background: '#cfcdc6'}}
+              cover={<img alt="example" src={BookCover} />}
+            />
+          </Col>
+          <Col>
+          <Title>
+              {props.book_title}
+          </Title>
+          </Col>
+          </Row>
+          <WhiteSpace/>
           <p>{props.book_synopsis}</p>
           <p>{props.book_publisher}</p>
           <p>{props.publication_date}</p>
